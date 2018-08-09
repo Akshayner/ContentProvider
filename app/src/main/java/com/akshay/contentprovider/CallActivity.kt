4b33cd5c474320448905b84241e0d7f7bcc74c3a
nit <akshay.gurnule@gmail.com>
2018-08-09 12:13:34 +0530

    all files

--- app/src/main/java/com/akshay/contentprovider/CallActivity.kt
+++ app/src/main/java/com/akshay/contentprovider/CallActivity.kt
@@ -2,11 +2,20 @@ package com.akshay.contentprovider
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CallLog
+import android.provider.ContactsContract
+import android.widget.SimpleCursorAdapter
+import kotlinx.android.synthetic.main.activity_call.*
 
 class CallActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_call)
+
+        var call = contentResolver
+        var c = call.query(CallLog.Calls.CONTENT_URI, null, null, null, null)
+        var mya = SimpleCursorAdapter(this,R.layout.cview,c, arrayOf(CallLog.Calls.DURATION,CallLog.Calls.NUMBER), intArrayOf(R.id.tv1,R.id.tv2),0)
+        lv.adapter = mya
     }
 }
