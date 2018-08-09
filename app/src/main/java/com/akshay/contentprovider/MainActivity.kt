14e5fbffdcabc561d31647a7caccf531d7433bc0
nit <akshay.gurnule@gmail.com>
2018-08-09 11:40:08 +0530

    all files

--- app/src/main/java/com/akshay/contentprovider/MainActivity.kt
+++ app/src/main/java/com/akshay/contentprovider/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.akshay.contentprovider
 
-import android.content.ContentProvider
+
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract
-import android.widget.ArrayAdapter
-import android.widget.SimpleCursorAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,35 +12,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var cp = contentResolver
-        var c = cp.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
-        var mya = SimpleCursorAdapter(this@MainActivity,R.layout.cview,c, arrayOf(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,ContactsContract.CommonDataKinds.Phone.NUMBER), intArrayOf(R.id.tv1,R.id.tv2),0 )
-        lv.adapter = mya
-
-        ///// all data is shown on list
-
-        val list = mutableListOf<String>()
-        while(c.moveToNext()) {
-            val index = c.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
-            list.add(c.getString(index))
+        contact.setOnClickListener {
+            val i= Intent(this,ContactActivity::class.java)
+            startActivity(i)
         }
 
-        /// got list with contact names
-
-        var myad = ArrayAdapter<String>(this,android.R.layout.simple_list_item_single_choice,list)
-        actv.setAdapter(myad)
-        actv.threshold = 1
-
-        /// set contact names to the AutocompleteTextview
-
-
-        b1.setOnClickListener {
-            var cp = contentResolver
-            var c = cp.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME+"=?", arrayOf(actv.text.toString()),ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
-            var mya = SimpleCursorAdapter(this@MainActivity,R.layout.cview,c, arrayOf(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,ContactsContract.CommonDataKinds.Phone.NUMBER), intArrayOf(R.id.tv1,R.id.tv2),0 )
-            lv.adapter = mya
-
-
+        msz.setOnClickListener {
+            val i= Intent(this,MessageActivity::class.java)
+            startActivity(i)
         }
+
+        call.setOnClickListener {
+            val i= Intent(this,CallActivity::class.java)
+            startActivity(i)
+        }
+
     }
 }
